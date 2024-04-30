7be5a90e4837fb9037f01bb7efff5ab8eb48242c
eucleiance <eucleiance@gmail.com>
2024-04-30 14:51:46 +0545

    fixed reset functionality for needles

--- script.js
+++ script.js
@@ -2,57 +2,15 @@ const hourEl = document.querySelector(".hour");
 const minuteEl = document.querySelector(".minute");
 const secondEl = document.querySelector(".second");
 const timeEl = document.querySelector(".time");
-const dateEl = document.querySelector(".date");
 const toggleEl = document.querySelector(".toggle");
 
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",];
-
-toggleEl.addEventListener("click", (e) => {
-  const html = document.querySelector("html");
-  if (html.classList.contains("dark")) {
-    html.classList.remove("dark");
-    e.target.innerHTML = "Dark Mode";
-  } else {
-    html.classList.add("dark");
-    e.target.innerHTML = "Light Mode";
-  }
-})
-
-function setTime() {
-  const time = new Date()
-  const month = time.getMonth()
-  const day = time.getDay()
-  const date = time.getDate()
-  const hours = time.getHours()
-  const hoursForClock = hours % 12   // 12 Hour format
-  const minutes = time.getMinutes()
-  const seconds = time.getSeconds()
-  const ampm = hours >= 12 ? 'PM' : 'AM'
-  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
-  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
-  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
-
-  timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds} ${ampm}`
-  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
-}
-
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-setTime()
-setInterval(setTime, 1000)
-
 // Stopwatch
-const display = document.getElementById("display");
 let timer = null;
 let startTime = 0;
 let elapsedTime = 0;
 let isRunning = false;
 
 function start() {
-
   if (!isRunning) {
     startTime = Date.now() - elapsedTime;
     timer = setInterval(update, 10);
@@ -60,7 +18,6 @@ function start() {
   }
 }
 function stop() {
-
   if (isRunning) {
     clearInterval(timer);
     elapsedTime = Date.now() - startTime;
@@ -73,7 +30,15 @@ function reset() {
   startTime = 0;
   elapsedTime = 0;
   isRunning = false;
-  display.textContent = "00:00:00:00";
+  timeEl.innerHTML = `00:00:00:00`
+
+  hourEl.style.transform = null;
+  minuteEl.style.transform = null;
+  secondEl.style.transform = null;
+
+  // hourEl.style.transform = "translate(-50%, -100%) rotate(0deg);"
+  // minuteEl.style.transform = "translate(-50%, -100%) rotate(0deg);"
+  // secondEl.style.transform = "translate(-50%, -100%) rotate(0deg);"
 }
 
 function update() {
@@ -88,12 +53,29 @@ function update() {
   minutes = String(minutes).padStart(2, "0");
   seconds = String(seconds).padStart(2, "0");
   milliseconds = String(milliseconds).padStart(2, "0");
+  milliseconds = milliseconds.slice(-2);
 
-  display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hours, 0, 11, 0, 360)}deg)`
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+  timeEl.innerHTML = `${hours}:${minutes}:${seconds}:${milliseconds}`
 }
 
 
+toggleEl.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark Mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light Mode";
+  }
+})
 
 
-
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
