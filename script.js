797b3f6f2c34b96113cb829de8b9fe15a05b7cc5
eucleiance <eucleiance@gmail.com>
2024-05-01 17:24:16 +0545

    selects, timers & other things

--- script.js
+++ script.js
@@ -48,6 +48,9 @@ let elapsedTime = 0;
 let isRunning = false;
 
 // Pomodoro
+let pomo_timer = 5;
+let pomo_timer_c = 0;
+
 let work_block = 0;
 let break_block = 0;
 let f_rating = "Distracted"            // Distracted / OK / Focused / Flow
@@ -58,10 +61,90 @@ let focus_rating_i = "Flow";
 // pomo(run_time_i, total_run_time_i, focus_rating_i);
 
 
+
 for (let i = 1; i < 99; i += 0.5) {
   console.log(i, "mins")
   pomo(i, i, focus_rating_i);
 }
+
+function test() {
+  let pomo_timer_c = Date.now() + (pomo_timer * 60 * 1000)
+  let time_in_mins = (pomo_timer_c - Date.now()) / 60000
+  console.log(Date.now())
+  console.log(time_in_mins)
+}
+
+function start() {
+  if (!isRunning) {
+    startTime = Date.now() - elapsedTime;
+    timer = setInterval(update, 10);
+    isRunning = true;
+  }
+}
+function stop() {
+  if (isRunning) {
+    clearInterval(timer);
+    elapsedTime = Date.now() - startTime;
+    isRunning = false;
+  }
+}
+
+function reset() {
+  clearInterval(timer);
+  startTime = 0;
+  elapsedTime = 0;
+  isRunning = false;
+  timeEl.innerHTML = `00:00:00:00`
+
+  hourEl.style.transform = null;
+  minuteEl.style.transform = null;
+  secondEl.style.transform = null;
+
+  // hourEl.style.transform = "translate(-50%, -100%) rotate(0deg);"
+  // minuteEl.style.transform = "translate(-50%, -100%) rotate(0deg);"
+  // secondEl.style.transform = "translate(-50%, -100%) rotate(0deg);"
+}
+
+
+function update() {
+  const currentTime = Date.now();
+  elapsedTime = currentTime - startTime;
+  let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+  let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+  let seconds = Math.floor(elapsedTime / 1000 % 60);
+  let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+  hours = String(hours).padStart(2, "0");
+  minutes = String(minutes).padStart(2, "0");
+  seconds = String(seconds).padStart(2, "0");
+  milliseconds = String(milliseconds).padStart(2, "0");
+  milliseconds = milliseconds.slice(-2);
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hours, 0, 11, 0, 360)}deg)`
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+  timeEl.innerHTML = `${hours}:${minutes}:${seconds}:${milliseconds}`
+
+}
+
+
+toggleEl.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark Mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light Mode";
+  }
+})
+
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 
 
 
@@ -158,75 +241,8 @@ function break_block_calc(r_time) {
   }
 }
 
-
-
-function start() {
-  if (!isRunning) {
-    startTime = Date.now() - elapsedTime;
-    timer = setInterval(update, 10);
-    isRunning = true;
-  }
-}
-function stop() {
-  if (isRunning) {
-    clearInterval(timer);
-    elapsedTime = Date.now() - startTime;
-    isRunning = false;
-  }
-}
-
-function reset() {
-  clearInterval(timer);
-  startTime = 0;
-  elapsedTime = 0;
-  isRunning = false;
-  timeEl.innerHTML = `00:00:00:00`
-
-  hourEl.style.transform = null;
-  minuteEl.style.transform = null;
-  secondEl.style.transform = null;
-
-  // hourEl.style.transform = "translate(-50%, -100%) rotate(0deg);"
-  // minuteEl.style.transform = "translate(-50%, -100%) rotate(0deg);"
-  // secondEl.style.transform = "translate(-50%, -100%) rotate(0deg);"
-}
-
-function update() {
-  const currentTime = Date.now();
-  elapsedTime = currentTime - startTime;
-  let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-  let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
-  let seconds = Math.floor(elapsedTime / 1000 % 60);
-  let milliseconds = Math.floor(elapsedTime % 1000 / 10);
-
-  hours = String(hours).padStart(2, "0");
-  minutes = String(minutes).padStart(2, "0");
-  seconds = String(seconds).padStart(2, "0");
-  milliseconds = String(milliseconds).padStart(2, "0");
-  milliseconds = milliseconds.slice(-2);
-
-  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hours, 0, 11, 0, 360)}deg)`
-  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
-  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
-
-  timeEl.innerHTML = `${hours}:${minutes}:${seconds}:${milliseconds}`
-
-}
-
-
-toggleEl.addEventListener("click", (e) => {
-  const html = document.querySelector("html");
-  if (html.classList.contains("dark")) {
-    html.classList.remove("dark");
-    e.target.innerHTML = "Dark Mode";
-  } else {
-    html.classList.add("dark");
-    e.target.innerHTML = "Light Mode";
-  }
-})
-
-
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+// Rating Function 
+// function rating() {
+//   console.log("yo", selectedValue)
+// }
 
